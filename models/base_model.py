270073921f63b8cad585ecbdaf90a444eac074c7
Mahmoud <elmahallawy6@gmail.com>
2024-08-02 09:01:30 +0300

    mahmoud

--- models/base_model.py
+++ models/base_model.py
@@ -1,70 +1,52 @@
 #!/usr/bin/python3
-"""Defines the BaseModel class."""
-import models
-from uuid import uuid4
+""" Basmodel module for whole the program """
 from datetime import datetime
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column
-from sqlalchemy import DateTime
-from sqlalchemy import String
-
-Base = declarative_base()
+from uuid import uuid4
+import models
 
 
 class BaseModel:
-    """Defines the BaseModel class.
-
-    Attributes:
-        id (sqlalchemy String): The BaseModel id.
-        created_at (sqlalchemy DateTime): The datetime at creation.
-        updated_at (sqlalchemy DateTime): The datetime of last update.
-    """
-
-    id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    """ Base model class represents the parent class of the HBNB project """
 
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel.
-
-        Args:
-            *args (any): Unused.
-            **kwargs (dict): Key/value pairs of attributes.
+        """
+        constructor
+        args: unused
+        kwargs: attribute and its value k, v concept
         """
         self.id = str(uuid4())
-        self.created_at = self.updated_at = datetime.utcnow()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        time_f = '%Y-%m-%dT%H:%M:%S.%f'
         if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__":
-                    setattr(self, key, value)
+            for k, v in kwargs.items():
+                if k != '__class__':
+                    if (k == 'created_at' or k == 'updated_at'):
+                        v = datetime.strptime(v, time_f)
+                    else:
+                        setattr(self, k, v)
+        else:
+            models.storage.new(self)
 
     def save(self):
-        """Update updated_at with the current datetime."""
-        self.updated_at = datetime.utcnow()
-        models.storage.new(self)
+        """ method used to update the updated date of the object """
+        self.updated_at = datetime.today()
         models.storage.save()
 
-    def to_dict(self):
-        """Return a dictionary representation of the BaseModel instance.
-
-        Includes the key/value pair __class__ representing
-        the class name of the object.
-        """
-        my_dict = self.__dict__.copy()
-        my_dict["__class__"] = str(type(self).__name__)
-        my_dict["created_at"] = self.created_at.isoformat()
-        my_dict["updated_at"] = self.updated_at.isoformat()
-        my_dict.pop("_sa_instance_state", None)
-        return my_dict
-
-    def delete(self):
-        """Delete the current instance from storage."""
-        models.storage.delete(self)
-
     def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
-        d = self.__dict__.copy()
-        d.pop("_sa_instance_state", None)
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, d)
+        """
+        return the user-readable string for the name of the class
+        and its unique id
+        and a dictionary containing all attributes
+        """
+        form_at = "[{}] ({}) {}"
+        cl_name = self.__class__.__name__
+        return form_at.format(cl_name, self.id, self.__dict__)
+
+    def to_dict(self):
+        """ return dictionary representation of the object """
+        dic_t = dict(self.__dict__)
+        dic_t["__class__"] = self.__class__.__name__
+        dic_t["created_at"] = self.created_at.isoformat()
+        dic_t["updated_at"] = self.updated_at.isoformat()
+        return dic_t
