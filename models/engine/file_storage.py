a4cbf7fb251e62d34e4d147c9079ac5f28a00288
Mahmoud <elmahallawy6@gmail.com>
2024-08-02 09:10:20 +0300

    mahmoud

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,59 +1,65 @@
 #!/usr/bin/python3
-"""
-class file storage
-"""
-import json
+""" FileStorage that serializes instances to a JSON file and
+deserializes JSON file to instances """
 import os.path
 from models.base_model import BaseModel
+import json
 from models.user import User
 from models.state import State
 from models.city import City
-from models.amenity import Amenity
 from models.place import Place
+from models.amenity import Amenity
 from models.review import Review
 
 
 class FileStorage:
-    """
-    inner class file storage
+    """class contain methods which
+    control sriliztion and deserializtion
+    file_path:
+    string of the path of the file in which objects stored"
+    objects Dictionary of object.
     """
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
-        """
-        methon all of file storage
-        """
-        return FileStorage.__objects
+    def all(self, cls=None):
+        """ return the dictionary of objects """
+        if cls is None:
+            return FileStorage.__objects
+        else:
+            new_dict = {}
+            for k, v in FileStorage.__objects.items():
+                if type(v) is cls:
+                    new_dict[k] = v
+            return new_dict
 
     def new(self, obj):
-        """
-        method new of file storage
-        """
-        FileStorage.__objects[obj.__class__.__name__ + "." + obj.id] = obj
+        """ method to set obj with key <obj class name>.id """
+        cl_name = obj.__class__.__name__
+        ke = cl_name + "." + obj.id
+        self.__objects.update({ke: obj})
 
     def save(self):
-        """
-        method save of file storage
-        """
-        first_dict = {}
-        for key, val in FileStorage.__objects.items():
-            if val:
-                first_dict[key] = val.to_dict()
-        with open(FileStorage.__file_path, 'w', encoding='utf-8') as f:
-            json.dump(first_dict, f)
+        """ serialize objects and save it in json file """
+        new_dict = {}
+        for k, v in self.__objects.items():
+            new_dict[k] = v.to_dict()
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
+            json.dump(new_dict, f)
 
     def reload(self):
-        """
-        method reload of class reload
-        """
-        my_dict = {'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                   'State': State, 'City': City, 'Amenity': Amenity,
-                   'Review': Review}
-        if os.path.isfile(FileStorage.__file_path):
-            with open(FileStorage.__file_path, 'r', encoding='utf-8') as q:
-                other_dict = json.loads(q.read())
-                for key, val in other_dict.items():
-                    self.new(my_dict[val['__class__']](**val))
-
+        """ method used to deserialize json to objects """
+        if os.path.isfile(self.__file_path):
+            with open (self.__file_path) as f:
+                obj_dic = json.load(f)
+            for dicts in obj_dic.values():
+                clas_name = dicts["__class__"]
+                del dicts["__class__"]
+                self.new(eval(clas_name)(**dicts))
     
+    def delete(self, obj=None):
+        """ delet obj from __objects"""
+        if (obj):
+            obj_k = obj.to_dict()["__class__"] + "." + obj.id
+            if obj_k in self.__objects.keys():
+                del(self.__objects[obj_k])
