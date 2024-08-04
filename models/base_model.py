4ba96506072e2840fb531e45170ec27f1b3c78aa
Mahmoud <elmahallawy6@gmail.com>
2024-08-04 10:16:41 +0300

    m

--- models/base_model.py
+++ models/base_model.py
@@ -16,22 +16,22 @@ class BaseModel:
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
-        else:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                if key != '__class__':
-                    setattr(self, key, value)
-            if 'id' not in kwargs:
-                self.id = str(uuid.uuid4())
-            if 'created_at' not in kwargs:
-                self.created_at = datetime.now()
-            if 'updated_at' not in kwargs:
-                self.updated_at = datetime.now()
+        """
+        constructor
+        args: unused
+        kwargs: attribute and its value k, v concept
+        """
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        time_f = '%Y-%m-%dT%H:%M:%S.%f'
+        if kwargs:
+            for k, v in kwargs.items():
+                if k != '__class__':
+                    if (k == 'created_at' or k == 'updated_at'):
+                        v = datetime.strptime(v, time_f)
+                    else:
+                        setattr(self, k, v)
 
     def __str__(self):
         """Returns a string representation of the instance"""
